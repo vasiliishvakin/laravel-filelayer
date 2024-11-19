b5d83daa4d8f1c8af87ba7b9ffe4654bb6b86f8d
vasiliishvakin <vasilii.shvakin@gmail.com>
2024-11-20 00:39:23 +0200

    fix

--- src/Generators/FileName/FileNameGeneratorByActions.php
+++ src/Generators/FileName/FileNameGeneratorByActions.php
@@ -21,29 +21,21 @@ class FileNameGeneratorByActions
 
     public function __construct()
     {
-        $this->prefix = config('filelayer.file_name_generator.prefix');
-        $this->noActionsName = config('filelayer.file_name_generator.no_actions_name');
-        $this->folder1Length = config('filelayer.file_name_generator.folder_1_length');
-        $this->folder2Length = config('filelayer.file_name_generator.folder_2_length');
-        $this->hashAlgorithm = config('filelayer.file_name_generator.hash_algorithm');
+        $this->prefix = config('filelayer.file_name_generator.prefix', 'processed');
+        $this->noActionsName = config('filelayer.file_name_generator.no_actions_name', 'original');
+        $this->folder1Length = config('filelayer.file_name_generator.folder_1_length', 1);
+        $this->folder2Length = config('filelayer.file_name_generator.folder_2_length', 1);
+        $this->hashAlgorithm = config('filelayer.file_name_generator.hash_algorithm', 'sha1');
     }
 
     public function __invoke(FileWrapper $file, array $actions): string
     {
-        $actionClassesString = empty($actions)
-            ? $this->noActionsName
-            : implode(
-                '_',
-                array_map(
-                    fn ($action) => Str::of($action)->classBasename()->lower()->replace(['-', '_'], ''),
-                    $actions
-                )
-            );
+        $actionClassesString = $this->actionsToPath($actions);
 
         $hashPath = Str::of($file->path())
             ->trim('/')
             ->lower()
-            ->pipe(fn ($path) => Str::of(hash($this->hashAlgorithm, (string) $path)));
+            ->pipe(fn($path) => Str::of(hash($this->hashAlgorithm, (string) $path)));
 
         $extension = $file->extension();
         $fileName = $hashPath->append('.')->append($extension);
@@ -51,6 +43,25 @@ class FileNameGeneratorByActions
         $folder_1 = $hashPath->substr(0, $this->folder1Length);
         $folder_2 = $hashPath->substr($this->folder1Length, $this->folder2Length);
 
-        return implode(DIRECTORY_SEPARATOR, [$this->prefix, $actionClassesString, $folder_1, $folder_2, $fileName]);
+        return implode(DIRECTORY_SEPARATOR, [$this->prefix(), $actionClassesString, $folder_1, $folder_2, $fileName]);
+    }
+
+    public function actionsToPath(array $actions): string
+    {
+        $actionClassesString = empty($actions)
+            ? $this->noActionsName
+            : implode(
+                '_',
+                array_map(
+                    fn($action) => Str::of($action)->classBasename()->lower()->replace(['-', '_'], ''),
+                    $actions
+                )
+            );
+        return $actionClassesString;
+    }
+
+    public function prefix(): string
+    {
+        return $this->prefix;
     }
 }
