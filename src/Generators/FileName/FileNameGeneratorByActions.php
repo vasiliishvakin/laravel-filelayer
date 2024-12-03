f502fe2fa8f963120bc40440e0bd4a26135c53b8
vasiliishvakin <vasilii.shvakin@gmail.com>
2024-12-03 18:09:50 +0200

    allow add suffix (like version) to processors

--- src/Generators/FileName/FileNameGeneratorByActions.php
+++ src/Generators/FileName/FileNameGeneratorByActions.php
@@ -44,7 +44,7 @@ class FileNameGeneratorByActions
         $extension = $file instanceof FileWrapper ? $file->extension() : Path::getExtension($file, true);
         $hash = hash(self::hashAlgorithm(), $name);
 
-        return $hash.'.'.$extension;
+        return $hash . '.' . $extension;
     }
 
     public static function hashPath(string|FileWrapper $file): string
@@ -54,7 +54,7 @@ class FileNameGeneratorByActions
         $extension = $file instanceof FileWrapper ? $file->extension() : Path::getExtension($file, true);
         $hash = hash(self::hashAlgorithm(), $path);
 
-        return $hash.'.'.$extension;
+        return $hash . '.' . $extension;
     }
 
     public function __invoke(string|FileWrapper $file, array $actions, ?string $subprefix = null): ?string
@@ -85,7 +85,21 @@ class FileNameGeneratorByActions
         return implode(
             '_',
             array_map(
-                fn (string $action) => Str::of($action)->classBasename()->swap($this->reducingClasses)->lower()->swap(['_' => '', '-' => ''])->toString(),
+                function (string $action) {
+                    if (!class_exists($action)) {
+                        throw new \InvalidArgumentException("Action class $action does not exist");
+                    }
+                    $classId =  Str::of($action)
+                        ->classBasename()
+                        ->swap($this->reducingClasses)
+                        ->lower()
+                        ->swap(['_' => '', '-' => ''])
+                        ->toString();
+                    if (method_exists($action, 'suffix')) {
+                        return $classId . strtoupper($action::suffix());
+                    }
+                    return $classId;
+                },
                 $actions
             )
         );
